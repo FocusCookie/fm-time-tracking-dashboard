0fd58d111973ef8262b32e79d3a6230eb415f958
FocusCookie <stephan_lueddemann@web.de>
2021-09-27 16:53:30 +0200

    Finished activiy component

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const user = document.getElementsByTagName("custom-user")[0];
+const work = document.getElementById("work");
 
 user.addEventListener("button", (e) => {
   user.setAttribute("active", e.detail);
   console.log("LOAD DATASET FOR - ", e.detail);
 });
+
+setTimeout(() => {
+  work.timeframes = { current: 5, previous: 7 };
+}, 3000);
---
+++ components/activity/activity.component.js
@@ -0,0 +1,52 @@
+fetch("/components/activity/activity.component.html")
+  .then((stream) => stream.text())
+  .then((text) => define(text));
+
+function define(html) {
+  class ActivityComponent extends HTMLElement {
+    constructor() {
+      super();
+
+      this._timeframes;
+
+      this.attachShadow({ mode: "open" });
+      this.shadowRoot.innerHTML = html;
+
+      const card = this.shadowRoot.getElementById("card");
+      card.style.backgroundImage = `url(${this.getAttribute("icon")})`;
+
+      const background = this.getAttribute("background");
+      const activityColors = {
+        work: "var(--color-work)",
+        play: "var(--color-play)",
+        study: "var(--color-study)",
+        exercise: "var(--color-exercise)",
+        social: "var(--color-social)",
+        selfcare: "var(--color-selfcare)",
+      };
+
+      card.style.backgroundColor = activityColors[background]
+        ? activityColors[background]
+        : background;
+
+      const title = this.getAttribute("title");
+      const titleElement = this.shadowRoot.getElementById("title");
+      titleElement.innerHTML = title;
+    }
+
+    set timeframes(timeframes) {
+      this._timeframes = timeframes;
+
+      const current = this.shadowRoot.getElementById("current");
+      const last = this.shadowRoot.getElementById("last");
+
+      current.innerHTML = `${timeframes.current}hrs`;
+      last.innerHTML = `Last Week - ${timeframes.previous}hrs`;
+    }
+    get timeframes() {
+      return this._timeframes;
+    }
+  }
+
+  customElements.define("custom-activity", ActivityComponent);
+}
