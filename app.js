72b33063959b23aa95b878b0c44652a1daca7c18
FocusCookie <stephan_lueddemann@web.de>
2021-09-28 14:24:05 +0200

    All timeframes are loaded from the data.json and feeded into the corosponding activiy card

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const user = document.getElementsByTagName("custom-user")[0];
-const work = document.getElementById("work");
-let timeframes;
+const activityElements = {
+  work: document.getElementById("work"),
+  play: document.getElementById("play"),
+  study: document.getElementById("study"),
+  exercise: document.getElementById("exercise"),
+  social: document.getElementById("social"),
+  selfcare: document.getElementById("selfcare"),
+};
 
 readJsonFile = async function (path) {
   try {
@@ -14,17 +20,10 @@ readJsonFile = async function (path) {
 };
 
 readJsonFile("./data.json")
-  .then((data) => {
-    timeframes = data;
-
-    console.log(
-      timeframes.filter((elem) => elem.title === "Work")[0].timeframes
-    );
-
-    work.timeframes = timeframes.filter(
-      (elem) => elem.title === "Work"
-    )[0].timeframes;
-    //TODO: filter each timeframe into each element - create elements on top like the work one
+  .then((timeframes) => {
+    timeframes.forEach((activity) => {
+      activityElements[activity.title].timeframes = activity.timeframes;
+    });
   })
   .catch((err) => console.log(err));
 
@@ -32,5 +31,3 @@ user.addEventListener("button", (e) => {
   user.setAttribute("active", e.detail);
   console.log("LOAD DATASET FOR - ", e.detail);
 });
-
-//TODO: Handling the clicks on the user type and switch data in the custom avtivity cards
